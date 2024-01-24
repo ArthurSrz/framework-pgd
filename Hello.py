e8f0d068a7101431f570ea74c40b317c849f00ae
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2024-01-24 13:22:28 +0000

    backwards

--- Hello.py
+++ Hello.py
@@ -68,8 +68,8 @@ def run():
         
         layout = [
             # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
-            dashboard.Item("first_item", 0, 0, 2, 2, isResizable=True, isDraggable=True),
-            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=True, moved=True),
+            dashboard.Item("first_item", 0, 0, 2, 2),
+            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=True, moved=False),
             dashboard.Item("third_item", 0, 2, 1, 1, isResizable=True),
         ]    
 
@@ -97,39 +97,7 @@ def run():
                         ]
                     )
                 ]
-            with mui.Card:
-                key = "8"
-                sx={"maxWidth": 345},
-                children=[
-                    mui.CardMedia(
-                        sx={"height": 140},
-                        image="https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    ),
-                    mui.CardContent("Card content"),
-                    mui.CardActions(
-                        [
-                            mui.Button(size="small", children="Share"),
-                            mui.Button(size="small", children="Learn More"),
-                        ]
-                    )
-                ]
-            with mui.Card:
-                key = "8"
-                sx={"maxWidth": 345},
-                children=[
-                    mui.CardMedia(
-                        sx={"height": 140},
-                        image="https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    ),
-                    mui.CardContent("Card content"),
-                    mui.CardActions(
-                        [
-                            mui.Button(size="small", children="Share"),
-                            mui.Button(size="small", children="Learn More"),
-                        ]
-                    )
-                ]
-            
+            mui.Card("First item", key="first_item")
             mui.Card("Second item", key="second_item")
             mui.Card("Third item", key="third_item")
             
