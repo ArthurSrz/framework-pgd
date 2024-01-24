b1a8c426df86934f272e4c49a80df5416d7386e9
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2024-01-24 12:38:22 +0000

    first class

--- Hello.py
+++ Hello.py
@@ -15,6 +15,7 @@
 import streamlit as st
 from streamlit.logger import get_logger
 from streamlit_elements import dashboard
+from streamlit_elements import elements, mui, html
 
 LOGGER = get_logger(__name__)
 
@@ -36,8 +37,32 @@ def run():
     """
     )
     
-    
+    with elements("dashboard"):
+        # First, build a default layout for every element you want to include in your dashboard
 
+        layout = [
+            # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+            dashboard.Item("first_item", 0, 0, 2, 2),
+            dashboard.Item("second_item", 2, 0, 2, 2, isDraggable=False, moved=False),
+            dashboard.Item("third_item", 0, 2, 1, 1, isResizable=False),
+        ]    
+
+        # Next, create a dashboard layout using the 'with' syntax. It takes the layout
+        # as first parameter, plus additional properties you can find in the GitHub links below.
+
+        with dashboard.Grid(layout):
+            mui.Paper("First item", key="first_item")
+            mui.Paper("Second item (cannot drag)", key="second_item")
+            mui.Paper("Third item (cannot resize)", key="third_item")
+        
+        def handle_layout_change(updated_layout):
+            # You can save the layout in a file, or do anything you want with it.
+            # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+            print(updated_layout)
+        with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+            mui.Paper("First item", key="first_item")
+            mui.Paper("Second item (cannot drag)", key="second_item")
+            mui.Paper("Third item (cannot resize)", key="third_item")
 
 if __name__ == "__main__":
     run()
