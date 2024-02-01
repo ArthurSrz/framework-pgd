6a21c32fe0aafa7cbadebaeb19ac9e8182e90fa8
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2024-02-01 15:47:10 +0000

    retry

--- pages/1_adaptation.py
+++ pages/1_adaptation.py
@@ -90,8 +90,7 @@ def run():
                 mui.CardActions(
                         mui.Button(
                             #open link in new tab
-                            ahref="https://google.com",
-                            rel="opener referrer",
+                            href="https://google.com",
                             variant="contained",
                             size="small",
                             children="Editer",
